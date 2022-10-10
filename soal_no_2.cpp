2380d418cac143d9dcd14943a687d30aab952ede
mariogabema <marioparasian@gmail.com>
2022-10-10 20:29:50 +0700

    first commit

---
+++ soal_no_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Contoh Program Perulangan";
+
+    for (a = 1; a <= 10; a++)
+    {
+
+        cout << endl;
+
+        cout << a;
+    }
+
+    cout << "\n perulangan menurun";
+
+    for (b = 10; b >= 1; b--)
+    {
+
+        cout << endl;
+
+        cout << b;
+    }
+}
